b8694eba1c741aae5c60f1b9e4893207389dc824
zhangzhiqiang <zhangzhiqiang@nonobank.com>
2019-01-24 14:51:42 +0800

    update

--- src/main/scala/com/test/ReadCsvFile.scala
+++ src/main/scala/com/test/ReadCsvFile.scala
@@ -9,6 +9,7 @@ object ReadCsvFile {
   def main(args: Array[String]): Unit = {
     val env = ExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(1)
+    // 直接将数据，转成Student（相当于Schema）
     val values = env.readCsvFile[Student]("/Users/zhangzhiqiang/Documents/test_project/flinkdemo/data/1.csv")
     values.print()
   }

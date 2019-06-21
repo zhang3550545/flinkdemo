f64c0e9bc335bf552692bf40bbb609df9670c966
ZHANGZHIQIANG002 <ZHANGZHIQIANG002@huazhu.com>
2019-06-21 11:37:00 +0800

    update

--- src/main/java/com/test/operator/JoinAndCoGroupOperator.java
+++ src/main/java/com/test/operator/JoinAndCoGroupOperator.java
@@ -3,6 +3,7 @@ package com.test.operator;
 import com.google.gson.Gson;
 import com.test.bean.Order;
 import com.test.bean.User;
+import org.apache.flink.api.common.functions.CoGroupFunction;
 import org.apache.flink.api.common.functions.JoinFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
@@ -14,24 +15,37 @@ import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTime
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.triggers.ProcessingTimeTrigger;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
+import org.apache.flink.util.Collector;
 import scala.Tuple7;
 
 import java.util.Properties;
 
 /**
- * 测试数据
+ * join 测试数据
  * <p>
  * User{userId='1001', name='caocao', age='20', sex='null', createTime=1561023040338, updateTime=1561023040338}
  * Order{orderId='1001', userId='1001', price='10', timestamp=1561023042640}
  * Order{orderId='1002', userId='1001', price='20', timestamp=1561023043649}
  * Order{orderId='1003', userId='1002', price='30', timestamp=1561023044651}
  * order join user> (1001,caocao,20,1001,1001,10,1561023042640)
- * user join order> (1001,caocao,20,1001,1001,10,1561023042640)
  * order join user> (1001,caocao,20,1001,1002,20,1561023043649)
- * user join order> (1001,caocao,20,1001,1002,20,1561023043649)
+ * <p>
+ * <p>
+ * coGroup测试数据
+ * <p>
+ * User{userId='1001', name='caocao', age='20', sex='male', createTime=1561087784197, updateTime=1561087784197}
+ * Order{orderId='1001', userId='1001', price='10', timestamp=1561087786816}
+ * Order{orderId='1002', userId='1001', price='20', timestamp=1561087787831}
+ * Order{orderId='1003', userId='1002', price='30', timestamp=1561087788832}
+ * ---------------------------------
+ * [Order{orderId='1001', userId='1001', price='10', timestamp=1561087786816}, Order{orderId='1002', userId='1001', price='20', timestamp=1561087787831}]
+ * [User{userId='1001', name='caocao', age='20', sex='male', createTime=1561087784197, updateTime=1561087784197}]
+ * ---------------------------------
+ * [Order{orderId='1003', userId='1002', price='30', timestamp=1561087788832}]
+ * []
  */
 
-public class JoinOperator {
+public class JoinAndCoGroupOperator {
 
     public static void main(String[] args) throws Exception {
 
@@ -84,30 +98,31 @@ public class JoinOperator {
                 })
                 .print("order join user");
 
-
-        operator1.join(operator)
-                .where(new KeySelector<User, String>() {
-                    @Override
-                    public String getKey(User value) throws Exception {
-                        return value.userId;
-                    }
-                })
-                .equalTo(new KeySelector<Order, String>() {
+        // coGroup既会输出匹配的记过，也会输出未匹配的结果，给出的方式，一个迭代器，需要自己组装。这是和join的区别
+        operator.coGroup(operator1)
+                .where(new KeySelector<Order, String>() {
                     @Override
                     public String getKey(Order value) throws Exception {
                         return value.userId;
                     }
+                })
+                .equalTo(new KeySelector<User, String>() {
+                    @Override
+                    public String getKey(User value) throws Exception {
+                        return value.userId;
+                    }
                 }).window(TumblingProcessingTimeWindows.of(Time.minutes(1), Time.seconds(30)))
                 .trigger(ProcessingTimeTrigger.create())
-                .apply(new JoinFunction<User, Order, Tuple7<String, String, String, String, String, String, Long>>() {
+                .apply(new CoGroupFunction<Order, User, Tuple7<String, String, String, String, String, String, Long>>() {
                     @Override
-                    public Tuple7<String, String, String, String, String, String, Long> join(User user, Order order) throws Exception {
-                        return new Tuple7<>(user.userId, user.name, user.age, order.userId, order.orderId, order.price, order.timestamp);
+                    public void coGroup(Iterable<Order> first, Iterable<User> second, Collector<Tuple7<String, String, String, String, String, String, Long>> out) throws Exception {
+                        System.out.println("---------------------------------");
+                        System.out.println(first);
+                        System.out.println(second);
                     }
-                })
-                .print("user join order");
+                }).print("coGroup");
 
 
-        sEnv.execute("JoinOperator");
+        sEnv.execute("JoinAndCoGroupOperator");
     }
 }
